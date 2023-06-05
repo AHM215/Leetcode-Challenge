156154cf409da545da78e913b42765818b85b9d7
Ahmed Hagras <87371291+AHM215@users.noreply.github.com>
2023-06-06 00:54:26 +0300

    Time: 576 ms (7.00%), Space: 165 MB (8.86%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -2,21 +2,20 @@ class Solution {
 private:
     vector<vector<int>> res;
     void search(int n, int k, vector<int> presult, int index){
-        if(presult.size()==k){
-            res.push_back(presult);
+        if(presult.size()==k){ // Base case
+            res.push_back(presult); 
             return;
         }
-        for(int i=index;i<n;i++){
-            presult.push_back(i+1);
-            search(n, k, presult, i+1);
-            presult.pop_back();
+        for(int i=index;i<n;i++){ 
+            presult.push_back(i+1); // add the current element to the combination
+            search(n, k, presult, i+1); // recursively call the function with the next index
+            presult.pop_back(); // remove the current element from the combination
         }
-        
     }
 public:
     vector<vector<int>> combine(int n, int k) {
-        vector<int> presult;
-        search(n, k, presult, 0);
+        vector<int> presult; 
+        search(n, k, presult, 0); 
         return res;
     }
 };
