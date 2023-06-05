8e649fd549ec970a79aa918e62f7a00b8f3ed3dd
Ahmed Hagras <87371291+AHM215@users.noreply.github.com>
2023-06-05 22:05:55 +0300

    Time: 6 ms (71.33%), Space: 9.6 MB (84.17%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    vector<string> letterCasePermutation(const string& s) {
-        vector<string> ans;
-        dfs(s, 0, ans);
-        return ans;
+    vector<string> letterCasePermutation(string s) {
+        vector<string> ans; 
+        dfs(s, 0, ans);     
+        return ans;       
     }
     
 private:
-    void dfs(const string& s, unsigned short pos, vector<string>& ans) {
-        if (pos == s.length()) {
-            ans.push_back(s);
-            return;
+    void dfs(string& s, int pos, vector<string>& ans) {
+        if (pos == s.length()) {    // Base case
+            ans.push_back(s);      
+            return;              
         }
         
-        dfs(s, pos + 1, ans);
-        if (isalpha(s[pos])) {
-            string t(s);
-            t[pos] ^= (1 << 5);  // toggle case
-            dfs(t, pos + 1, ans);
+        if (isalpha(s[pos])) {      // If the current character is a letter
+            s[pos] ^= (1 << 5);     // Toggle the case of the letter by XORing with 1 << 5
+            dfs(s, pos + 1, ans);   // Recursively generate permutations with the modified string
+            s[pos] ^= (1 << 5);     // Toggle the case back to its original state
         }
+        
+        dfs(s, pos + 1, ans);       // Recursively generate permutations without modifying the current character
     }
 };
+
+/*
+Time complexity: O(n * 2^n), since we need to generate 2^n permutations, and for each permutation, we need to iterate through the entire string of length n to check which characters are letters.
+
+Space complexity: O(n * 2^n), where n is the length of the input string. This is because we generate 2^n permutations, and each permutation can be up to length n.
+*/
