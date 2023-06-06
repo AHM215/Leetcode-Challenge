3cb367d625fa5f45385566f5a001d6565927b83b
Ahmed Hagras <87371291+AHM215@users.noreply.github.com>
2023-06-06 22:56:08 +0300

    Time: 127 ms (54.63%), Space: 6 MB (53.15%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int count = 0;
+    
+    // Function to count the number of valid arrangements
     int countArrangement(int N) {
-        vector<bool> visited(N + 1, false);
-        calculate(N, 1, visited);
-        return count;
+        vector<bool> visited(N + 1, false); 
+        calculate(N, 1, visited);        
+        return count;                        
     }
+    
     void calculate(int N, int pos, vector<bool>& visited) {
-        if (pos > N)
-            count++;
-        for (int i = 1; i <= N; i++) {
-            if (!visited[i] && (pos % i == 0 || i % pos == 0)) {
-                visited[i] = true;
-                calculate(N, pos + 1, visited);
-                visited[i] = false;
+        if (pos > N) {   // Base cace
+            count++;             
+        }
+        for (int i = 1; i <= N; i++) {        // Loop over all possible numbers
+            if (!visited[i] && (pos % i == 0 || i % pos == 0)) {  // Check if the number is valid
+                visited[i] = true;           // Mark the number as visited
+                calculate(N, pos + 1, visited);  // Generate the next permutation recursively
+                visited[i] = false;          // Unmark the number as visited
             }
         }
     }
