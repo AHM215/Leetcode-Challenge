9ca8042469782b1cc830b84daa18ec70efd896fb
Ahmed Hagras <87371291+AHM215@users.noreply.github.com>
2023-06-25 10:43:21 +0300

    Time: 142 ms (37.67%), Space: 14.5 MB (69.96%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX); // initialize DP array with maximum values
+        vector<int> dp(amount + 1, amount + 1); // initialize DP array with maximum values
         dp[0] = 0; // base case: it takes 0 coins to make 0 amount
         sort(coins.rbegin(), coins.rend());
         for (int i = 1; i <= amount; i++) { // loop over all amounts from 1 to amount
             for (int j = 0; j < coins.size(); j++) { // loop over all coin denominations
                 // if the current coin can be used to make up the current amount
                 // and using the current coin leads to a valid solution
-                if (coins[j] <= i && dp[i - coins[j]] != INT_MAX) {
+                if (coins[j] <= i && dp[i - coins[j]] != amount + 1) {
                     dp[i] = min(dp[i], dp[i - coins[j]] + 1); 
                     // take the minimum of the current DP value and the new DP value
                 }
             }
         }
-        return dp[amount] == INT_MAX ? -1 : dp[amount]; // return the final DP value
+        return dp[amount] == amount + 1 ? -1 : dp[amount]; // return the final DP value
     }
 };
