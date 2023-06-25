8936cc722c1861e291f667a495e7c18770e1969a
Ahmed Hagras <87371291+AHM215@users.noreply.github.com>
2023-06-25 10:42:19 +0300

    Time: 119 ms (55.22%), Space: 14.5 MB (69.96%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,6 +3,7 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount + 1, INT_MAX); // initialize DP array with maximum values
         dp[0] = 0; // base case: it takes 0 coins to make 0 amount
+        sort(coins.rbegin(), coins.rend());
         for (int i = 1; i <= amount; i++) { // loop over all amounts from 1 to amount
             for (int j = 0; j < coins.size(); j++) { // loop over all coin denominations
                 // if the current coin can be used to make up the current amount
