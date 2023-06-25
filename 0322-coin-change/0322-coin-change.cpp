75a78e620aa7cc170f99c710e2ce003750f3fcda
Ahmed Hagras <87371291+AHM215@users.noreply.github.com>
2023-06-25 10:36:05 +0300

    Time: 163 ms (25.85%), Space: 14.4 MB (83.31%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    int f(int i,int t,vector<int>& coins,vector<vector<int>>&dp){
-        if(i==0){
-            if(t%coins[0]==0) return t/coins[0];
-            else return 1e9;
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX); // initialize DP array with maximum values
+        dp[0] = 0; // base case: it takes 0 coins to make 0 amount
+        for (int i = 1; i <= amount; i++) { // loop over all amounts from 1 to amount
+            for (int j = 0; j < coins.size(); j++) { // loop over all coin denominations
+                // if the current coin can be used to make up the current amount
+                // and using the current coin leads to a valid solution
+                if (coins[j] <= i && dp[i - coins[j]] != INT_MAX) {
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1); 
+                    // take the minimum of the current DP value and the new DP value
+                }
+            }
         }
-        if(dp[i][t]!=-1) return dp[i][t];
-        int nt=0+f(i-1,t,coins,dp);
-        int tt=1e9;
-        if(coins[i]<=t){
-            tt=1+f(i,t-coins[i],coins,dp);
-        }
-        return dp[i][t]=min(tt,nt);
-    }
-    int coinChange(vector<int>& coins, int t) {
-        int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(t+1,-1));
-        int x=f(n-1,t,coins,dp);
-        if(x!=1e9) return x;
-        else return -1;
+        return dp[amount] == INT_MAX ? -1 : dp[amount]; // return the final DP value
     }
 };
