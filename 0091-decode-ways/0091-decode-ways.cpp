6f2a1e8cc247850a100dbb5739d304ebaa2d2018
Ahmed Hagras <87371291+AHM215@users.noreply.github.com>
2023-06-25 10:21:26 +0300

    Time: 0 ms (100.00%), Space: 6.3 MB (43.72%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        vector<int> dp(s.size()+1);
-        dp[0]=1;
-        if(s[0]=='0')dp[1]=0;
-        else dp[1]=1;
-        for(int i=2;i<=s.size();i++){
-            int way1,way2;
-            if(s[i-1]=='0')way1=0;
-            else way1=dp[i-1];
-            if(stoi(s.substr(i-2,2))<=26&&stoi(s.substr(i-2,2))>0&&s[i-2]!='0')way2=dp[i-2];
-            else way2=0;
-            dp[i]=way1+way2;
+        int n = s.length(); // get the length of the input string
+        vector<int> dp(n+1); // create a vector to store the DP values
+        dp[0] = 1; // set the base case dp[0] to 1
+        dp[1] = s[0] != '0' ? 1 : 0; // set the base case dp[1] based on the first digit of s
+        for (int i = 2; i <= n; i++) { // loop over all indices of the input string
+            if (s[i-1] != '0') { // if the current digit is not zero
+                dp[i] += dp[i-1]; // add the previous DP value
+            }
+            if (s[i-2] == '1' || (s[i-2] == '2' && s[i-1] <= '6')) { // if the current and previous digits form a valid two-digit code
+                dp[i] += dp[i-2]; // add the DP value two indices back
+            }
         }
-        return dp[s.size()];
+        return dp[n]; // return the final DP value
     }
 };
