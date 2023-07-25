f4552f5ae7babf225caf695b1afb9ca1bae587ff
Ahmed Hagras <87371291+AHM215@users.noreply.github.com>
2023-07-25 13:57:19 +0300

    Time: 144 ms (69.63%), Space: 69.5 MB (46.43%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,26 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         bool flag = false;
+//         for(int i =0;i<nums.size()-1;i++){
+//             if(nums[i] == nums[i+1]) return true;
+//         }
+//         return flag;
+//     }
+// };
+
+// solve with map
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        bool flag = false;
-        for(int i =0;i<nums.size()-1;i++){
-            if(nums[i] == nums[i+1]) return true;
+        unordered_map<int, int> count;
+        for (int i = 0; i < nums.size(); i++) {
+            if (count[nums[i]] > 0) {
+                return true;
+            }
+            count[nums[i]]++;
         }
-        return flag;
+        return false;
     }
 };
